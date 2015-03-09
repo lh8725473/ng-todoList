475e48ff50dc83c58352b91d35ebe19c9e21aa24
liuh <171213574@qq.com>
2015-03-09 21:33:53 +0800

    fixed bug

--- js/todoList.js
+++ js/todoList.js
@@ -6,16 +6,15 @@ angular.module('todoListApp').controller('todoListAppController',[
     $scope.todoList = [];
 
     $scope.add = function(){
+      if($scope.txt === '' || $scope.txt == undefined){
+        alert('请填写todo内容')
+      }
       var todo = {
         txt: $scope.txt,
         checked: false
       }
       $scope.todoList.push(todo)
       $scope.txt = ''
-    }
-
-    $scope.finish = function(todo){
-      todo.checked = !todo.checked
     }
   }
 ])
